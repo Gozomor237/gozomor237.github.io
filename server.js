c9287b9905ab81657492566c47b56fbadae083f7
Gozomor237 <terridellirossy6@gmail.com>
2026-06-07 23:05:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // backend Agrossignol v2
 const express = require('express');
-const express = require('express');const express = require('express');
 const cors = require('cors');
 const app = express();
 
